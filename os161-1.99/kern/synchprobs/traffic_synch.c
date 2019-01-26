aace3bd90b9c516f5f5cc0431dbade67633fd59e
jl2ma <jl2ma@student.cs.uwaterloo.ca>
2019-01-26 16:40:53 -0500

    <Added init and destroy for traffic>

--- os161-1.99/kern/synchprobs/traffic_synch.c
+++ os161-1.99/kern/synchprobs/traffic_synch.c
@@ -22,7 +22,12 @@
  * replace this with declarations of any synchronization and other variables you need here
  */
 static struct semaphore *intersectionSem;
+static struct cv *cv_left;
+static struct cv *cv_straight;
 
+static struct lock *lk_left;
+static struct lock *lk_straight;
+static struct lock *lk_origin;
 
 /* 
  * The simulation driver will call this function once before starting
@@ -40,6 +45,32 @@ intersection_sync_init(void)
   if (intersectionSem == NULL) {
     panic("could not create intersection semaphore");
   }
+
+  cv_left = cv_create("left");
+  if (cv_left == NULL) {
+    panic("could not create cv left");
+  } 
+
+  cv_straight = cv_create("straight");
+  if (cv_straight == NULL) {
+    panic("could not create cv straight");
+  }
+
+  lk_left = lock_create("left");
+  if (lk_left == NULL) {
+    panic("could not create lk left");
+  }
+ 
+  lk_straight = lock_create("straight");
+  if (lk_straight == NULL) {
+    panic("could not create lk straight");
+  }
+
+  lk_origin = lock_create("origin");
+  if (lk_origin == NULL) {
+    panic("could not create lk origin");
+  } 
+    
   return;
 }
 
@@ -56,6 +87,21 @@ intersection_sync_cleanup(void)
   /* replace this default implementation with your own implementation */
   KASSERT(intersectionSem != NULL);
   sem_destroy(intersectionSem);
+
+  KASSERT(cv_left != NULL);
+  cv_destroy(cv_left);
+
+  KASSERT(cv_straight != NULL);
+  cv_destroy(cv_straight);
+
+  KASSERT(lk_left != NULL);
+  lock_destroy(lk_left);
+
+  KASSERT(lk_straight != NULL);
+  lock_destroy(lk_straight);
+
+  KASSERT(lk_origin != NULL);
+  lock_destroy(lk_origin);
 }
 
 
