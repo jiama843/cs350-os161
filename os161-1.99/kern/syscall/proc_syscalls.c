fbe2484f71eeafc38f25720d4cdcf24bcc307c19
john <jiama843@gmail.com>
2019-03-09 10:10:45 -0500

    <Fixing userptr_copy>

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -85,10 +85,10 @@ static size_t userptr_copy(userptr_t u_old, userptr_t *u, size_t args_len){
 		}
 
 		size_t curr_len = strlen(str) + 1;
-    u[i] = kmalloc(curr_len);
+    &u[i] = kmalloc(curr_len);
 
     // TODO: Figure out why this is being bullied
-		err = copyoutstr(str, u[i], curr_len, NULL);
+		err = copyoutstr(str, &u[i], curr_len, NULL);
 		if(err){
 			panic("Copy outstr is bullying me in userptr_copy");
 		}
