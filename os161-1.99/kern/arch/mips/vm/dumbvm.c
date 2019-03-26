7c2e15eb058171da2d20ad28b5274ba2d5025026
john <jiama843@gmail.com>
2019-03-26 00:22:19 -0400

    <Read only text segment - loading with dirty bit after load elf (as activate)>

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -215,6 +215,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 	if(as->done_load_elf){
 		elo &= ~TLBLO_DIRTY;
 	}
+	
 	DEBUG(DB_VM, "dumbvm: 0x%x -> 0x%x\n", faultaddress, paddr);
 	tlb_random(ehi, elo);
 
--- os161-1.99/kern/syscall/loadelf.c
+++ os161-1.99/kern/syscall/loadelf.c
@@ -303,6 +303,7 @@ load_elf(struct vnode *v, vaddr_t *entrypoint)
 
 	*entrypoint = eh.e_entry;
 	as->done_load_elf = true;
+	as_activate();
 
 	return 0;
 }
