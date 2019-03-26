3f6a50c80460ba7495fc7cfcd48f44a988725154
john <jiama843@gmail.com>
2019-03-26 01:11:51 -0400

    <Read only text segment - loading with dirty bit after load elf>

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -197,7 +197,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 		elo = paddr | TLBLO_DIRTY | TLBLO_VALID;
 
 		// Add elo if load elf has completed
-		kprintf("DONE LOADING: %d", as->done_load_elf);
+		//kprintf("DONE LOADING: %d", as->done_load_elf);
 		if(as->done_load_elf && ehi >= vbase1 && ehi < vtop1){
 			elo &= ~TLBLO_DIRTY;
 		}
@@ -212,9 +212,9 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 	ehi = faultaddress;
 	elo = paddr | TLBLO_DIRTY | TLBLO_VALID;
 	
-	if(as->done_load_elf && ehi >= vbase1 && ehi < vtop1){
+	/*if(as->done_load_elf && ehi >= vbase1 && ehi < vtop1){
 		elo &= ~TLBLO_DIRTY;
-	}
+	}*/
 
 	DEBUG(DB_VM, "dumbvm: 0x%x -> 0x%x\n", faultaddress, paddr);
 	tlb_random(ehi, elo);
