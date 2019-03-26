abd8e169c6d57f7d9ad09959af7e99f81a90397c
john <jiama843@gmail.com>
2019-03-25 23:29:52 -0400

    <Read only text segment - return error code in vm fault>

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -122,7 +122,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 	    case VM_FAULT_READONLY:
 			/* We always create pages read-write, so we can't get this */
 			//panic("dumbvm: got VM_FAULT_READONLY\n");
-				return EX_MOD;
+				//return ;
 	    case VM_FAULT_READ:
 	    case VM_FAULT_WRITE:
 		break;
