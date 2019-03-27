ed5b8e5537578e93458c96486d699734bb6fd71a
john <jiama843@gmail.com>
2019-03-27 10:05:08 -0400

    <Physical memory management - debug memory out of range>

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -84,7 +84,7 @@ vm_bootstrap(void)
 	kprintf("PAGE_SIZE: %d", PAGE_SIZE);
 	kprintf("TOTAL FRAMES: %d", coremap->total_frames);
 
-	//coremap->map;
+	coremap->map = (int *) PADDR_TO_KVADDR(firstaddr + sizeof(coremap));
 	for(int i = 0; i < coremap->total_frames; i++){
 		coremap->map[i] = 0;
 	}
