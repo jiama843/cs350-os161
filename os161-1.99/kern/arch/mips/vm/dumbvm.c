f9a44429d4a56ba980707b02103541718e5835c5
john <jiama843@gmail.com>
2019-03-27 17:49:52 -0400

    <Physical memory management - improving printf>

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -119,7 +119,7 @@ getppages(unsigned long npages)
 			}
 
 			if(!can_alloc){
-				//i += npages;
+				i += npages;
 				continue;
 			}
 
@@ -177,7 +177,7 @@ alloc_kpages(int npages)
 			}
 
 			if(!can_alloc){
-				//i += npages - 1;
+				i += npages - 1;
 				continue;
 			}
 
