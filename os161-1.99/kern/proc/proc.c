343f1acc0edc4fe8ba6d1ece52984eb31fdfd5b6
john <jiama843@gmail.com>
2019-03-27 23:10:37 -0400

    <Adding assignment2b - There is literally nothing wrong with segmentation>

--- os161-1.99/kern/proc/proc.c
+++ os161-1.99/kern/proc/proc.c
@@ -182,9 +182,9 @@ proc_destroy(struct proc *proc)
 	}
 #endif // UW
 
-	while(threadarray_num(&proc->p_threads) != 0){
+	/*while(threadarray_num(&proc->p_threads) != 0){
 		threadarray_remove(&proc->p_threads, 0);
-	}
+	}*/
 
 	threadarray_cleanup(&proc->p_threads);
 	spinlock_cleanup(&proc->p_lock);
