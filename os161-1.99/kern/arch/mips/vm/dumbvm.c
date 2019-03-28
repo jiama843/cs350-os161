e780f9ce6de4648bc0ed27f27be020bfbee16bbc
john <jiama843@gmail.com>
2019-03-27 21:07:27 -0400

    <Adding assignment2b - include lock fix comp errors>

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -54,7 +54,7 @@
  */
 static struct spinlock stealmem_lock = SPINLOCK_INITIALIZER;
 
-static struct lock coremap_lock = lock_create("core_lock");
+static struct lock * coremap_lock = lock_create("core_lock");
 
 static struct coremap* coremap; /* Store coremap */
 
