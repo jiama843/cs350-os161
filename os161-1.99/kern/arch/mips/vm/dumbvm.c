7510cf00d4aa5dbbcc665ba9dc0f2331eda3ee71
john <jiama843@gmail.com>
2019-04-05 15:18:46 -0400

    <Paging - Fixed pgtable asserts>

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -467,7 +467,7 @@ as_prepare_load(struct addrspace *as)
 {
 	KASSERT(as->as_ptable1 != NULL);
 	KASSERT(as->as_ptable2 != NULL);
-	KASSERT(as->as_pstacktable == NULL);
+	//KASSERT(as->as_pstacktable == NULL);
 
 	paddr_t pbase1 = getppages(as->as_npages1);
 	for(size_t i = 0; i < as->as_npages1; i++){
